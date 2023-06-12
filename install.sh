bcc2f854c09092d04f5302a942cfc623c9d60e81
Julian Weiß <6249046+xR3b0rn@users.noreply.github.com>
2023-06-12 17:39:54 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -12,4 +12,4 @@ cd .. && rm -r neovim
 git clone --depth 1 https://github.com/wbthomason/packer.nvim\
  ~/.local/share/nvim/site/pack/packer/start/packer.nvim
 
-mkdir -p ~/.config && git clone git@github.com:xR3b0rn/nvim-setup.git ~/.config/nvim
+mkdir -p ~/.config && git clone https://github.com/xR3b0rn/nvim-setup.git ~/.config/nvim

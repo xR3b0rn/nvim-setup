73ad319eb4aa1a525f715c4198d75480bcba037a
Julian Weiß <weiss@compredict.de>
2023-06-15 14:03:56 +0200

    Added cmake4vim and vimspector

---
+++ after/plugin/cmake4vim.lua
@@ -0,0 +1,13 @@
+
+vim.g.cmake_vimspector_support = 1
+vim.g.cmake_vimspector_default_configuration = {
+  adapter = "vscode-cpptools",
+  configuration = {
+    request = "launch",
+    cwd = "${workspaceRoot}",
+    Mimode = "",
+    args = {},
+    program = ""
+  }
+}
+

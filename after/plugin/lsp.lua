52541da10b59043a5a9fa2221a9706daafd473bd
Julian Weiß <j.weiss@primes.de>
2025-06-06 11:09:23 +0200

    simplified lsp setup

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -1,29 +1,21 @@
-local lsp = require("lsp-zero")
-local lspconfig = require("lspconfig")
 local mason = require("mason")
-local cmp_nvim_lsp = require("cmp_nvim_lsp")
 local wk = require("which-key")
+local cmp = require('cmp')
+local cmp_nvim_lsp = require("cmp_nvim_lsp")
+local ls = require("lsp_signature")
+
+mason.setup({})
 
 vim.opt.digraph = false
 
-lsp.extend_lspconfig()
-lspconfig.lua_ls.setup({
-  settings = {
-    Lua = {
-      diagnostics = {
-        globals = { "vim" }
-      }
-    }
-  }
-})
-
-local cmp = require('cmp')
-local cmp_select = { behavior = cmp.SelectBehavior.Select }
+vim.lsp.enable("clangd")
+vim.lsp.enable("lua_ls")
+vim.lsp.enable("cmake-language-server")
 
 cmp.setup({
   mapping = {
-    ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
-    ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
+    ['<C-p>'] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Select }),
+    ['<C-n>'] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Select }),
     ['<C-y>'] = cmp.mapping.confirm({ select = true }),
     ['<C-Space>'] = cmp.mapping.complete(),
   },
@@ -36,96 +28,60 @@ cmp.setup({
   },
 })
 
-mason.setup({})
-
-lsp.on_attach(function(client, bufnr)
-  wk.add({
-    {
-      mode = "n",
-      buffer = bufnr,
-      { "gD",          vim.lsp.buf.declaration,      desc = "Goto declaration" },
-      { "gd",          vim.lsp.buf.definition,       desc = "Goto definition" },
-      { "K",           vim.lsp.buf.hover,            desc = "Hover" },
-      { "<leader>vws", vim.lsp.buf.workspace_symbol, desc = "Workspace symbol" },
-      { "<leader>vd",  vim.diagnostic.open_float,    desc = "Open float" },
-      { "[d",          vim.diagnostic.goto_next,     desc = "Next diagnostic" },
-      { "]d",          vim.diagnostic.goto_prev,     desc = "Prev diagnostic" },
-      { "<leader>vca", vim.lsp.buf.code_action,      desc = "Code action" },
-      { "<leader>vrr", vim.lsp.buf.references,       desc = "List references" },
-      { "<leader>vrn", vim.lsp.buf.rename,           desc = "Rename symbol" },
-    }
-  })
-  wk.add({
-    {
-      mode   = { "n", "v" },
-      buffer = bufnr,
-      { "<C-f>", "<cmd>lua vim.lsp.buf.format({ async = true })<CR>", desc = "Format code" },
-    }
-  })
-  wk.add({
-    {
-      mode   = "i",
-      buffer = bufnr,
-      { "<C-h>", vim.lsp.buf.signature_help, desc = "Signature help" },
-    }
-  })
-  local ls = require("lsp_signature")
-  wk.add({
-    {
-      mode = "i",     -- Insert mode
-      buffer = bufnr, -- Make it buffer-local
-      noremap = true,
-      silent = true,
-      { "<C-k>", ls.toggle_float_win, desc = "Toggle LSP signature window" }
-    }
-  })
-
-  ls.on_attach({
-    bind = true,
-    handler_opts = { border = "rounded" },
-  }, bufnr)
-end)
-
-
 vim.diagnostic.config({
   virtual_text = true
 })
 
-lsp.configure("clangd", {
+vim.lsp.config("clangd", {
   capabilities = cmp_nvim_lsp.default_capabilities(),
   cmd = {
     "clangd",
     "--offset-encoding=utf-16",
     "--clang-tidy",
   },
-  root_dir = function()
-    return vim.fn.getcwd()
-  end,
-})
-lsp.configure("rust_analyzer", {
-  settings = {
-    ["rust-analyzer"] = {
-      imports = {
-        granularity = {
-          group = "module",
-        },
-        prefix = "self",
-      },
-      cargo = {
-        buildScripts = {
-          enable = true,
-        },
-      },
-      procMacro = {
-        enable = true,
-      },
-    },
-  },
-  on_new_config = function(new_config, new_cwd)
-    print("test")
-    local status, cmake = pcall(require, "cmake-tools")
-    if status then
-      cmake.clangd_on_new_config(new_config)
-    end
-  end,
+  on_attach = function(client, bufnr)
+    wk.add({
+      {
+        mode = "n",
+        buffer = bufnr,
+        { "gD",          vim.lsp.buf.declaration,      desc = "Goto declaration" },
+        { "gd",          vim.lsp.buf.definition,       desc = "Goto definition" },
+        { "K",           vim.lsp.buf.hover,            desc = "Hover" },
+        { "<leader>vws", vim.lsp.buf.workspace_symbol, desc = "Workspace symbol" },
+        { "<leader>vd",  vim.diagnostic.open_float,    desc = "Open float" },
+        { "[d",          vim.diagnostic.goto_next,     desc = "Next diagnostic" },
+        { "]d",          vim.diagnostic.goto_prev,     desc = "Prev diagnostic" },
+        { "<leader>vca", vim.lsp.buf.code_action,      desc = "Code action" },
+        { "<leader>vrr", vim.lsp.buf.references,       desc = "List references" },
+        { "<leader>vrn", vim.lsp.buf.rename,           desc = "Rename symbol" },
+      }
+    })
+    wk.add({
+      {
+        mode   = { "n", "v" },
+        buffer = bufnr,
+        { "<C-f>", "<cmd>lua vim.lsp.buf.format({ async = true })<CR>", desc = "Format code" },
+      }
+    })
+    wk.add({
+      {
+        mode   = "i",
+        buffer = bufnr,
+        { "<C-h>", vim.lsp.buf.signature_help, desc = "Signature help" },
+      }
+    })
+    wk.add({
+      {
+        mode = "i",
+        buffer = bufnr,
+        noremap = true,
+        silent = true,
+        { "<C-k>", ls.toggle_float_win, desc = "Toggle LSP signature window" }
+      }
+    })
+    ls.on_attach({
+      bind = true,
+      handler_opts = { border = "rounded" },
+    }, bufnr)
+  end
 })

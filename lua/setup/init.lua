dad6efb3b9c03ce3be37309d136e55c1b660dc4a
Julian Weiß <j.weiss@primes.de>
2025-10-13 19:57:02 +0200

    refactoroed folder structure

--- lua/setup/init.lua
+++ lua/setup/init.lua
@@ -1,2 +1,61 @@
-require("setup.remap")
-require("setup.set")
+vim.o.mouse = 'a'
+vim.opt.guicursor = ""
+
+vim.opt.nu = true
+vim.opt.relativenumber = true
+
+vim.opt.tabstop = 2
+vim.opt.softtabstop = 2
+vim.opt.shiftwidth = 2
+vim.opt.expandtab = true
+
+vim.opt.smartindent = true
+
+vim.opt.wrap = false
+
+vim.opt.swapfile = false
+vim.opt.backup = false
+vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
+vim.opt.undofile = true
+
+vim.opt.hlsearch = true
+vim.opt.incsearch = true
+
+vim.opt.termguicolors = true
+
+vim.opt.scrolloff = 8
+vim.opt.signcolumn = "yes"
+vim.opt.isfname:append("@-@")
+
+vim.opt.updatetime = 50
+
+vim.opt.colorcolumn = "100"
+
+vim.opt.foldmethod = "expr"
+vim.opt.foldexpr = "nvim_treesitter#foldexpr()"
+vim.opt.foldlevel = 99
+
+vim.opt.list = true
+
+vim.opt.cursorline = true
+
+function ColorMyPencils(color)
+	color = color or "rose-pine"
+	vim.cmd.colorscheme(color)
+  hl = vim.api.nvim_get_hl(0, { name = "Normal" })
+  hl.blend = 100
+  hl.bg = "none"
+	vim.api.nvim_set_hl(0, "Normal", hl)
+	vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none"})
+end
+
+vim.g.mapleader = " "
+local wk = require("which-key")
+wk.add({
+  {
+    mode = { "n" },
+    { "<leader>pv", vim.cmd.Ex,        desc = "file exporer" },
+    { "<leader>gg", vim.cmd.LazyGit,   desc = "LazyGit" },
+    { "<leader>e",  '<cmd>lua vim.diagnostic.open_float()<CR>',   desc = "float diagnostics" },
+  }
+})
